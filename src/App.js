46fe9787ba78eba83bfc6ca244db5b6dfe6e9186
NeizarKevin <neizarkevin@gmail.com>
2022-12-05 15:34:46 +0700

    Upload File

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import NoteBody from "./components/NoteBody";
+import NoteHeader from "./components/NoteHeader";
+import NoteFooter from "./components/NoteFooter";
+import { getInitialData } from "./utils";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [searchNotes, setSearchNotes] = useState([]);
+  const [notes, setNotes] = useState(getInitialData());
+
+  const notesAll = (searchNotes || notes).filter((note) => !note.archived);
+  const notesArchive = (searchNotes || notes).filter((note) => note.archived);
+
+  useEffect(() => {
+    setSearchNotes(
+      notes.filter((note) =>
+        note.title.toLowerCase().includes(query.toLowerCase())
+      )
+    );
+  }, [query, notes]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="note-app">
+      <NoteHeader search={query} setQuery={setQuery} />
+      <NoteBody
+        notesAll={notesAll}
+        notesArchive={notesArchive}
+        setNotes={setNotes}
+      />
+      <NoteFooter />
     </div>
   );
 }
